92a44497a9070c19e825af141bf30c2bddee8ebd
Gptharohalli <GPTHAROHALLI198@gmail.com>
2024-08-26 12:32:18 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+#Aim:To write a python program to add 2 integers and 2 strings and print the result
+#programme:
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+x=num1+num2
+print("sum of given tow numbers is :",x)
+str1="hello"
+str2="mani"
+y=str1+str2
+print("sum of given tow strings is:",y)
